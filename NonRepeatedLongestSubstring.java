2a9f43fae9168ee211346e24f25758b9d960f5fc
sugandhaguru07 <sugandhaguru2002@gmail.com>
2025-11-19 19:06:13 +0530

    NonRepeatedLongestSubstring.java file is modified

--- NonRepeatedLongestSubstring.java
+++ NonRepeatedLongestSubstring.java
@@ -29,3 +29,32 @@ public class NonRepeatedLongestSubstring {
         return maxLen;
 }
 }
+
+/* But if we want to return the length of the substring as well as the substring itself then the code is--  */
+public class NonRepeatedLongestSubstring
+{
+    public String[] lengthOfLongestSubstring(String s)
+    {
+        HashSet<Character> hs=new HashSet<>();
+        int left=0,right=0,maxLen=0,start=0;
+        while(right<s.length())
+        {
+            if(!hs.contains(s.charAt(right)))
+            {
+                hs.add(s.charAt(right));
+                right++;
+                if(right-left>maxLen)
+                {
+                    maxLen=right-left;
+                    start=left;
+                }
+            }
+            else{
+                hs.remove(s.charAt(left));
+                left++;
+            }
+        }
+        String substr=s.substring(start,start+maxLen);
+        return new String[]{substr,String.valueOf(maxLen)};
+    }
+}
